dee1c70969a3e92add94491376718bf05b9cf09c
C.D. Clark III <clifton.clark@gmail.com>
2016-07-05 11:35:10 -0500

    minor reorg in the BeamConfigurator class

--- src/BeamConfigurator.hpp
+++ src/BeamConfigurator.hpp
@@ -58,24 +58,28 @@ BeamConfigurator::configure( GaussianBeam* beam )
 {
 
   if(this->hasWavelength())
+  {
     beam->setWavelength( this->getWavelength<t::nanometer>().value() );
 
-  // probably the most common configuration scenario
-  // the beam diameter and divergence are known.
-  if(this->hasWavelength() && this->hasDivergence() && this->hasDiameter())
-  {
-    quantity<t::centimeter> waistDiameter = 4.*beam->getWavelength<t::centimeter>() / quantity_cast<double>( M_PI*this->getDivergence<t::radian>().value() );
-    beam->setWaistDiameter( waistDiameter );
+    if(this->hasDivergence())
+    { // if the divergence is known, then we can determine the beam waist diameter
 
-    quantity<t::centimeter> pos = 0*cm;
-    if( this->hasPosition() )
-      pos = this->getPosition<t::centimeter>().value();
+      quantity<t::centimeter> waistDiameter = 4.*beam->getWavelength<t::centimeter>() / quantity_cast<double>( M_PI*this->getDivergence<t::radian>().value() );
+      beam->setWaistDiameter( waistDiameter );
 
-    quantity<t::centimeter> waistPosition = pos - beam->getRayleighRange<t::centimeter>() * sqrt( pow<2>(this->getDiameter<t::centimeter>().value()/beam->getWaistDiameter<t::centimeter>()) - 1. );
-    beam->setWaistPosition( waistPosition );
+      if(this->hasDiameter())
+      { // if the diameter at some location is known, then we can determine where the beam waist is
+
+        quantity<t::centimeter> pos = 0*cm;
+        if( this->hasPosition() )
+          pos = this->getPosition<t::centimeter>().value();
+
+        quantity<t::centimeter> waistPosition = pos - beam->getRayleighRange<t::centimeter>() * sqrt( pow<2>(this->getDiameter<t::centimeter>().value()/beam->getWaistDiameter<t::centimeter>()) - 1. );
+        beam->setWaistPosition( waistPosition );
+      }
 
 
-    return;
+    }
   }
 
 }
