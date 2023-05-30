1b8c982fe25eded85cb64bb8d2cc91f76e949f51
CD Clark III <clifton.clark@gmail.com>
2023-05-30 09:06:55 -0500

    housekeeping: switched to conanfile.txt file.

--- conanfile.py
+++
@@ -1,7 +0,0 @@
-from conans import ConanFile, CMake, tools
-import os
-
-class ConanBuild(ConanFile):
-    settings = "os", "arch", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain"
-    requires = 'boost/1.72.0','eigen/3.3.7', 'BoostUnitDefinitions/0.2@cd3/devel'
